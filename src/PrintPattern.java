02a52f1361f24f40a1a01b00dd10c36173e943fc
beethecoder <bas.gautam@gmail.com>
2022-03-10 21:08:27 +0100

    updated patterns

--- src/PrintPattern.java
+++ src/PrintPattern.java
@@ -1,7 +1,7 @@
 public class PrintPattern {
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 2; i++) {
+        for (int i = 0; i <= 5; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
